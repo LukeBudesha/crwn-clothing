f900f7f78522c97bf566145c94ee2cef5e912d99
luke budesha <lukebudesha@gmail.com>
2020-12-28 21:47:15 +0000

    integrate firebase and google sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAFAzcRdl_xpQhKuOsMQfY5wu6kWlTNq0s",
+    authDomain: "crwn-db-61f76.firebaseapp.com",
+    projectId: "crwn-db-61f76",
+    storageBucket: "crwn-db-61f76.appspot.com",
+    messagingSenderId: "97161209939",
+    appId: "1:97161209939:web:47f0c7737737d8b0867ea6"
+  }
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
